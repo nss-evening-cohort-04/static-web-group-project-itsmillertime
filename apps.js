784334dae0816b17666efee90fe59bbd1168368d
Max Baldwin <maxrbaldwin@gmail.com>
2016-08-04 17:36:46 -0500

    adding javascript

---
+++ apps.js
@@ -0,0 +1,52 @@
+var musicProducts = [
+  
+  {title: "Basic Package",
+    price: "$X",
+    description: "",
+    img: "product-basic.png"
+  },  
+
+  {title: "Sheet Music",
+    price: "$X",
+    description: "",
+    img: "product-sheetmusic.png"
+  },  
+
+  {title: "Full Band",
+    price: "$X",
+    description: "",
+    img: "product-basic.png"
+  },  
+
+  {title: "Instrumental",
+    price: "$X",
+    description: "",
+    img: "product-instrumental.png"
+  },  
+
+  {title: "CDs",
+    price: "$X",
+    description: "",
+    img: "product-cd.png"
+  },  
+
+  {title: "Songwriting Coaching",
+    price: "$X",
+    description: "",
+    img: "product-coaching.png"
+  },  
+
+  {title: "Skype Lessons",
+    price: "$X",
+    description: "",
+    img: "product-lesson.png"
+  },  
+
+  {title: "Attend the Coaching Lessons",
+    price: "$X",
+    description: "",
+    img: "product-session.png"
+  },  
+
+
+];
